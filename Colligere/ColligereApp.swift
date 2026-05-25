4faf87d5693762bae3519cf58203969dc5b60d32
Mark Perry <emailmarkperry@gmail.com>
2026-05-24 23:02:03 -0500

    Initial Commit

---
+++ Colligere/ColligereApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColligereApp.swift
+//  Colligere
+//
+//  Created by Mark Perry on 5/24/26.
+//
+
+import SwiftUI
+
+@main
+struct ColligereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Colligere/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Colligere
+//
+//  Created by Mark Perry on 5/24/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
